146fed62b00c244dd5309afae1651273105d12dd
Keali'i Transfield <liitransfield@gmail.com>
2021-02-23 16:22:25 -0700

    Added Dialog Branches to connect responses to

--- Untitled_Monster_Dating_App/Assets/Li'i/Scripts/ConversationTemplate.cs
+++ Untitled_Monster_Dating_App/Assets/Li'i/Scripts/ConversationTemplate.cs
@@ -7,7 +7,9 @@ public class ConversationTemplate : ScriptableObject
     [Header("Template of Monster that is speaking:")]
     public MonsterTemplate monsterTemplate;
     [TextArea(15,20)]
-    public string Dialog;
-    public string[] reponses = new string[] {"Yes [Example]", "No [Example]", "What does that mean? [Example]"};
+    public string Dialog = "Let's get spooky tonight. [Example]";
+    public string[] reponses = new string[] {"Yes. [Example]", "No. [Example]", "What does that mean? [Example]"};
     public int[] valueOfReponses = new int[] {1, -1, 0};
+    [Header("The Dialog each Response leads to:")]
+    public ConversationTemplate[] branchConversations = new ConversationTemplate[3];
 }
