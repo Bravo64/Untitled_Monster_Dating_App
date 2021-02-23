1902569e1282fb5c46840ed4044b4db7b2af7fc3
Keali'i Transfield <liitransfield@gmail.com>
2021-02-22 17:21:16 -0700

    Added new Monster Template Object

---
+++ Untitled_Monster_Dating_App/Assets/Li'i/Scripts/MonsterTemplate.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEditor.U2D;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "New Monster", menuName = "Create New Monster")]
+public class MonsterTemplate : ScriptableObject
+{
+    public Texture profileImage;
+    public string name;
+    public string username;
+    [TextArea(15,20)]
+    public string profileDescription;
+    public int relationshipScore = 10;
+    [UnityEngine.Range(0, 10)]
+    public int attraction = 5;
+    [UnityEngine.Range(0, 10)]
+    public int intelligence = 5;
+    [UnityEngine.Range(0, 10)]
+    public int humor = 5;
+    public bool isAVampire = false;
+    public string[] likes = new string[] {"Pizza [Example]", "Sushi [Example]", "Long walks [Example]"};
+    public string[] dislikes = new string[] {"Jogging [Example]", "Fish [Example]", "Stakes [Example]"};
+}
+
